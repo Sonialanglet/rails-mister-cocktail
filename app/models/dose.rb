2987ee78202bc53b54fe59e2bc7f3e7c26a2808d
Sonialanglet <slanglet@loeildumarketing.com>
2017-11-24 11:40:36 +0100

    mistercocktail vend matin

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,5 +1,5 @@
 class Dose < ApplicationRecord
   belongs_to :cocktail
   belongs_to :ingredient
-  validates :cocktail_id, uniqueness: { scope: [:ingredient_id] }
+  validates :ingredient_id, uniqueness: { scope: [:cocktail_id] }
 end
