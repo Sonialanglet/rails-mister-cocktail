95a7560af0041fbb7564d9de257493bafbccc967
Sonialanglet <slanglet@loeildumarketing.com>
2017-11-24 15:54:24 +0100

    suppression cocktails de seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,23 +14,11 @@ puts "creating ingredients"
 
 ingredient1 = Ingredient.create(name: "lemon")
 ingredient2 = Ingredient.create(name: "ice")
-ingredient3= Ingredient.create(name: "mint leaves")
+ingredient3 = Ingredient.create(name: "mint leaves")
 ingredient4 = Ingredient.create(name: "strawberry")
 ingredient5 = Ingredient.create(name: "banana")
 ingredient6 = Ingredient.create(name: "melon")
 
-puts "creating cocktails"
 
-cocktail1 = Cocktail.create(name: "mojito")
-cocktail2 = Cocktail.create(name: "sex on the beach")
-cocktail3 = Cocktail.create(name: "bludmarry")
-cocktail4 = Cocktail.create(name: "gin tonic")
-
-puts "creating doses"
-
-Dose.create(description: "toto", cocktail: cocktail1, ingredient: ingredient1)
-Dose.create(description: "toto", cocktail: cocktail2, ingredient: ingredient2)
-Dose.create(description: "toto", cocktail: cocktail3, ingredient: ingredient3)
-Dose.create(description: "toto", cocktail: cocktail4, ingredient: ingredient4)
 
 puts "finished"
